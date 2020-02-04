720b065fe5d623bc1654d7879fdbaaaa93276e9d
Jae Jin <jaej09@outlook.com>
2020-02-04 19:25:09 +0900

    #6 Mongoose's isNew Property

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -9,11 +9,17 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Create records", () => {
-  it("saves a user", () => {
+  it("saves a user", done => {
     // Whenever Mocha's sees it function, it's going to say ah the user or the developer is trying to run some type of test inside of this function right here.
     // 1. Create a new user
     const joe = new User({ name: "Joe" });
     // When we use it in combination with the new keyword and assign the result to Joe becomes an instance of user.
-    joe.save();
+    joe
+      .save() // save returns a promise
+      .then(() => {
+        // Has joe been saved successfully?
+        assert(!joe.isNew);
+        done();
+      });
   });
 });
--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -3,7 +3,17 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/users_test");
 mongoose.connection.once("open", () => console.log("Good to go!")).on("error", error => console.warn("Warning", error));
 
-beforeEach(() => {
+// before testing... execute it first.
+beforeEach(done => {
+  /**
+   * Mocha doesn't have any default idea of asynchronous operations.
+   * it just says, i'm going to run all my tests as fast as i can.
+   * we need to make sure that Mocha understands it needs to do a little bit of a pause before it runs the next test.
+   */
+
   // Take all the records inside of users collection and delete it.
-  mongoose.connection.collections.users.drop();
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next test!
+    done();
+  });
 });
