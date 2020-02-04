dd99845da15c33d00ef0a29fa6e4ed600650f0c8
Jae Jin <jaej09@outlook.com>
2020-02-04 12:18:48 +0900

    #6 The Basics of Mocha

---
+++ users/test/create_test.js
@@ -0,0 +1,12 @@
+/**
+ * Create a new user and save it to our database.
+ *
+ * So I'm going to call the function describe the described function takes two arguments.
+ * The first is a string that describes the test that we're about to write and then the second is a function.
+ */
+
+describe("Create records", () => {
+  it("saves a user", () => {
+    // Whenever Mocha's sees it function, it's going to say ah the user or the developer is trying to run some type of test inside of this function right here.
+  });
+});
