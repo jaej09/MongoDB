f48dc43ce322f145c5cfa231dc8c459579b7e5a7
Jae Jin <jaej09@outlook.com>
2020-02-04 23:05:00 +0900

    #6 Default Promise Implementation

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,7 +1,28 @@
 const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost/users_test");
-mongoose.connection.once("open", () => console.log("Good to go!")).on("error", error => console.warn("Warning", error));
+/**
+ * Difference between before and beforeEach is that
+ * before only is executed one time for all of your tests.
+ * in this case, we only need to tell mongoose to connect to Mongo exactly one time.
+ *
+ * So again we want to say to Mocha
+ * "Hey just hold up for a second pause execution and wait until we have successfully connected to Mongo."
+ */
+before(() => {
+  mongoose.connect("mongodb://localhost/users_test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  mongoose.connection
+    .once("open", () => {
+      done();
+    })
+    .on("error", error => {
+      console.warn("Warning", error);
+    });
+});
 
 // before testing... execute it first.
 beforeEach(done => {
