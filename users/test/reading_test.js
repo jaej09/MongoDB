51308626ad44876569b18bdc914e350575006e4b
Jae Jin <jaej09@outlook.com>
2020-02-05 00:17:21 +0900

    #6 The ID Property

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -6,13 +6,13 @@ describe("Reading users out of the database", () => {
 
   // Because a user data 'joe' created is removed from test_helper.js, we have to explicitly create another variable.
   beforeEach(done => {
-    joe = new User({ name: "Joe" });
+    joe = new User({ name: "Joe" }); // has _id property even though the data has not been saved yet.
     joe.save().then(() => done());
   });
 
   it("finds all users with a name of joe", done => {
     User.find({ name: "Joe" }).then(users => {
-      console.log(users);
+      assert(users[0]._id.toString() === joe.id.toString());
       done();
     });
   });
