aeab82629bebb8258ed0b78b1cfe006b7ac99ecf
Jae Jin <jaej09@outlook.com>
2020-02-04 19:00:07 +0900

    #6 Dropping Collections

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/users_test");
+mongoose.connection.once("open", () => console.log("Good to go!")).on("error", error => console.warn("Warning", error));
 
-mongoose.connection
-  .once("open", () => console.log("Good to go!"))
-  .on("error", error => {
-    console.warn("Warning", error);
-  });
+beforeEach(() => {
+  // Take all the records inside of users collection and delete it.
+  mongoose.connection.collections.users.drop();
+});
