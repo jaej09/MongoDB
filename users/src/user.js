a441148779c7ab8ec06a267d61faf9b231b7a0b2
Jae Jin <jaej09@outlook.com>
2020-02-04 12:04:12 +0900

    #6 Mongoose Models

---
+++ users/src/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: String
+});
+
+/**
+ * first argument (user) - what the collection is called inside of MongoDB.
+ * second argument (User Schema) - what we expect this collection of users to follow.
+ *                                 Anytime working with the user, we expect them to have a name and that name should be a string.
+ *
+ * variable name (User) - user class along with user model.
+ *                        It does not represent any particular user insight or application.
+ *                        It represents the entire collection of data that's sitting inside of our database.
+ */
+const User = mongoose.model("user", UserSchema);
+module.exports = User;
