5bc2c801be4aa644f72910c5fcc84326b89515c9
Jae Jin <jaej09@outlook.com>
2020-02-04 12:24:47 +0900

    #6 Running Mocha Tests

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -5,8 +5,11 @@
  * The first is a string that describes the test that we're about to write and then the second is a function.
  */
 
+const assert = require("assert");
+
 describe("Create records", () => {
   it("saves a user", () => {
     // Whenever Mocha's sees it function, it's going to say ah the user or the developer is trying to run some type of test inside of this function right here.
+    assert(1 + 5 === 2);
   });
 });
