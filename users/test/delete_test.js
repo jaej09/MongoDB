1b7a925708221e476793cbebb84f67beece4a80a
Jae Jin <jaej09@outlook.com>
2020-02-27 00:07:06 +0900

    #6 The Many Ways to Remove Records

---
+++ users/test/delete_test.js
@@ -0,0 +1,25 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Deleting a user", () => {
+  var joe;
+
+  beforeEach(done => {
+    joe = new User({ name: "Joe" }); // has _id property even though the data has not been saved yet.
+    joe.save().then(() => done());
+  });
+
+  it("model instance remove", done => {
+    // Remove joe first.
+    joe
+      .remove()
+      .then(() => {
+        // This .then only be executed once the remove operation is completed.
+        User.findOne({ name: "Joe" });
+      })
+      .then(user => {
+        assert(user === undefined);
+        done();
+      });
+  });
+});
