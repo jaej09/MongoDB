736edd92a00fae099b8bba38f90f4258a8f7808c
Jae Jin <jaej09@outlook.com>
2020-02-27 01:09:15 +0900

    #6 Set and Save for Updating Records

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -9,5 +9,28 @@ describe("Updating records", () => {
 		joe.save().then(() => done());
 	});
 
-	it("instance type using set and save", () => {});
+	it("instance type using set and save", () => {
+		// .set -> update name property of joe to Alex.
+		joe.set("name", "Alex");
+		console.log(joe);
+
+		// just setting a property however does not reflect that to the database.
+		// this is solely done in memory.
+
+		// .save can use after we have called .set on some property.
+		joe
+			.save()
+			.then(() => {
+				// collect the entire list of all the users in our collection.
+				// which means we use .find method.
+				// {} passing an empty object means I have no criteria for this find operation.
+				// just give me everything that's in here.
+				User.find({});
+			})
+			.then((users) => {
+				assert(users.length === 1);
+				assert(users[0].name === "Alex");
+				done();
+			});
+	});
 });
