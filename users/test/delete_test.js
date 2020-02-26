1c5b00dec60a9b79b9f7f1ae24e17cfe6a5b2bd6
Jae Jin <jaej09@outlook.com>
2020-02-27 00:27:18 +0900

    #6 Class Based Removes

--- users/test/delete_test.js
+++ users/test/delete_test.js
@@ -2,24 +2,33 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Deleting a user", () => {
-  var joe;
+	var joe;
 
-  beforeEach(done => {
-    joe = new User({ name: "Joe" }); // has _id property even though the data has not been saved yet.
-    joe.save().then(() => done());
-  });
+	beforeEach((done) => {
+		joe = new User({ name: "Joe" }); // has _id property even though the data has not been saved yet.\
+		joe.save().then(() => done());
+	});
 
-  it("model instance remove", done => {
-    // Remove joe first.
-    joe
-      .remove()
-      .then(() => {
-        // This .then only be executed once the remove operation is completed.
-        User.findOne({ name: "Joe" });
-      })
-      .then(user => {
-        assert(user === undefined);
-        done();
-      });
-  });
+	it("model instance remove", (done) => {
+		// Remove joe first.
+		joe
+			.remove()
+			.then(() => {
+				// This .then only be executed once the remove operation is completed.
+				User.findOne({ name: "Joe" });
+			})
+			.then((user) => {
+				assert(user === undefined);
+				done();
+			});
+	});
+
+	it("class method remove", (done) => {
+		User.remove({ name: "Joe" }).then(() => User.findOne({ name: "Joe" })).then((user) => {
+			assert(user === undefined);
+			done();
+		});
+	});
+
+	it("class method findOneAndRemove", () => {});
 });
